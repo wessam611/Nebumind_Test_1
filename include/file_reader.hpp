babe6f8bd93ffbd08bddce3ef8f410fdd1b5abe5
wessam611 <wessam611@live.com>
2021-04-07 12:48:06 +0200

    finalized project skeleton

---
+++ include/file_reader.hpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+/*
+    FileReader class for handling reading and writing .line and .csv
+    file while storing the data
+*/
+class FileReader{
+
+};
---
+++ include/table.hpp
@@ -0,0 +1,11 @@
+#include <string.h>
+#include <vector>
+
+using namespace std;
+
+/*
+    Table class for saving headers and content of a table
+*/
+class Table{
+
+};
