6fed4205bfc139541f5bda1df469486ac305df8f
wessam611 <wessam611@live.com>
2021-04-07 14:38:51 +0200

    running demo without reordering columns

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <string>
+
+#include "file_reader.hpp"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    cout << "main running" << endl;
+    string src, dst;
+    FileReader reader;
+
+    if (argc != 3){
+        cerr << "expected 2 arguments: src_file_path and dst_file_path" << endl;
+        return -1;
+    }
+    src = argv[1];
+    dst = argv[2];
+
+    Table table = reader.read_file(src, LINE);
+    reader.write_file(dst, table, CSV);
 }
